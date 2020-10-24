092d55c60491c6e61018c37561f1bd7829ca4fec
Diego Dleon Nunes <diegodnunes12@gmail.com>
2020-10-24 12:16:40 -0300

    Testando configuração de cors

--- proxy.config.js
+++ proxy.config.js
@@ -1,8 +1,8 @@
 const PROXY_CONFIG = [
     {
         context:['/api'],
-        target: 'https://newsapi.org/v2/top-headlines',
-        secure: false,
+        target: 'https://newsapi.org/v2/top-headlines/',
+        secure: true,
         logLevel: 'debug',
         pathRewrite: {'^/api' : ''}
     }
